41ed2b3ff6462b46cb63cc681741fd5a79f5cbe9
David F. <djfleming.metis@gmail.com>
2024-12-02 16:48:00 +0530

    Initial commit

---
+++ src/main/java/com/smeej/ManaBasedCrafter/ManaBasedCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.smeej.ManaBasedCrafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManaBasedCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManaBasedCrafterApplication.class, args);
+	}
+
+}
