663f8bf47e77333637383c4383d7ff63555a0dd4
David F. <djfleming.metis@gmail.com>
2024-12-05 21:28:52 +0530

    Search card command work in progress

---
+++ src/main/java/com/smeej/manabasedcrafter/commands/SearchCardCommand.java
@@ -0,0 +1,29 @@
+package com.smeej.manabasedcrafter.commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SearchCardCommand implements SlashCommand {
+    @Override
+    public String getName() {
+        return "search card";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+
+        String card = event.getOption("card")
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asString)
+                .get();
+
+        return event.reply()
+                // searchCard using "card" value
+                .withEphemeral(true)
+                .withContent("Searching MtG database for: " + card + "...");
+    }
+}
---
+++ src/main/java/com/smeej/manabasedcrafter/mtgservices/SearchCard.java
@@ -0,0 +1,5 @@
+package com.smeej.manabasedcrafter.mtgservices;
+
+public class SearchCard {
+    //use tolowercase and search using mtg api
+}
